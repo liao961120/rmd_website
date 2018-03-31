e5c68c26e0b7bd265405a5cd5c180c51243efd75
liao961120 <liaomovie2@gmail.com>
2018-03-31 21:05:05 +0800

    update

--- bootstrap_fc.R
+++ bootstrap_fc.R
@@ -26,14 +26,12 @@ aria_expanded=NULL, aria_controls=NULL) {
 }
 
 ################# Panel #####################
-panel <- function(content, title, color="default") {
+panel <- function(content, title, color = "default") {
     color <- paste("panel", paste("panel-", color, sep = ""))
-    div(class=color,
-        div(class="panel-heading",
-            h3(class="panel-title", title)
-            ),
-        div(class="panel-body", content)
-    )
+    div(class = color,
+        div(class = "panel-heading",
+            h3(class = "panel-title", title)),
+        div(class = "panel-body", content))
 }
 
 
@@ -41,7 +39,7 @@ panel <- function(content, title, color="default") {
 ### color: alert alert-success|nfo|warning|danger
 #
 ####### Collapse Button ##################
-### [Link](#id){.btn .btn-info role="button", data-toggle="collapse" aria-expanded="false" aria-controls="id"}
+### button_bs("Button_text", class="info", data_toggle="collapse", aria_expanded="false", aria_controls="id", data_target = "#id")
 ##########################################
 
 collapse <- function(content, id, color="well") {
@@ -52,25 +50,37 @@ collapse <- function(content, id, color="well") {
 
 ########### Modal ############
 ## size: modal-lg | modal-sm
-modal <- function(modal_header, modal_body, modal_body2=NULL, id, size=NULL) {
-    modal_size <- paste("modal-dialog", size, sep=" ")
-    
-    # modal
-    div(id=id, class="modal fade", role="dialog", tabindex="-1",
-        div(class=modal_size, role="document",
-            # modal content
-            div(class="modal-content",
-                div(class="modal-header",
-                    tags$button(type="button", class="close", `data-dismiss`="modal", HTML("&times;")),
-                    h4(class="modal-title", modal_header)
-                ),
-                div(class="modal-body",
-                    modal_body, modal_body2
-                ),
-                div(class="modal-footer",
-                    button_bs("Close", class="default", data_dismiss="modal")
+modal <-
+    function(modal_header, id, modal_body,
+             modal_body2 = NULL, size = NULL) {
+        modal_size <- paste("modal-dialog", size, sep = " ")
+        
+        # modal
+        div(id = id,
+            class = "modal fade",
+            role = "dialog",
+            tabindex = "-1",
+            div(class = modal_size,
+                role = "document",
+                # modal content
+                div(class = "modal-content",
+                    div(class = "modal-header",
+                        tags$button(type = "button",
+                                    class = "close",
+                                    `data-dismiss` = "modal",
+                                    HTML("&times;")
+                        ),
+                        h4(class = "modal-title", modal_header)
+                    ),
+                    div(class = "modal-body",
+                        modal_body, modal_body2),
+                    div(class = "modal-footer",
+                        button_bs("Close", class = "default",
+                                  data_dismiss = "modal")
+                    )
                 )
             )
         )
-    )
-}
+    }
+
+
